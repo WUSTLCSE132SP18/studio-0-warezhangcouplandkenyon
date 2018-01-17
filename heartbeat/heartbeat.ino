00f30e16dc7edcc9939fa0684374bbe8304aad02
Abigail Kenyon <Abigail Kenyon@DESKTOP-G1FS8IJ.dhcp.wustl.edu>
2018-01-17 13:53:16 -0600

    prints seconds

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,13 @@
+
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  double time = millis();
+  delay(1000);
+  //time = time + 1;
+  Serial.print (time/1000);
+  Serial.println(" seconds have elapsed");
+}
